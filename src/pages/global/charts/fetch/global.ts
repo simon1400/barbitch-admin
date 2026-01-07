778137cfd52408c8bc8c7c7f0fc53cc525971ed4
Dmytro Pechunka <pechunka11@gmail.com>
2026-01-07 11:29:11 +0100

    fixed buuld error

--- src/pages/global/charts/fetch/global.ts
+++ src/pages/global/charts/fetch/global.ts
@@ -5,11 +5,12 @@ import { getMonth } from 'date-fns'
 
 export const getGlobalStats = async () => {
   const globalStats = []
+  const currentYear = new Date().getFullYear()
   const currentMonth = getMonth(new Date())
   for (let i = 2; i <= currentMonth; i++) {
-    const dataAllWorks = await getAllWorks(i)
-    const dataCosts = await getMoney(i)
-    const dataAdmin = await getAdminsHours(i)
+    const dataAllWorks = await getAllWorks(i, currentYear)
+    const dataCosts = await getMoney(i, currentYear)
+    const dataAdmin = await getAdminsHours(i, currentYear)
     const item = {
       date: i + 1,
       flow: dataAllWorks.globalFlow,
